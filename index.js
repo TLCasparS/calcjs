ff7c75e8a248a84b2ee85a9c8eb076ec1de19924
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-12-23 19:30:28 +0100

    Add digits button with function
    all numbers can now be entered by buttons and typing
    equals button performs calculation

--- index.js
+++ index.js
@@ -1,51 +1,119 @@
 
 
 
+const digits = Array.from(document.querySelectorAll(".number"))
+let num1 = document.getElementById("num1")
+let num2 = document.getElementById("num2")
 
+
+let op;
+
+
+// put button digit input on first input field
+const putfDigit = function(){
+    digits.forEach(btn => {
+
+        btn.onclick = function(){
+            //num1.textContent += btn.innerHTML
+            num1.value = num1.value + btn.innerHTML
+            console.log(num1.value)
+        }
+        
+    });
+}
+
+// put button digit input on second input field
+const putsDigit = function(){
+    digits.forEach(btn => {
+
+        btn.onclick = function(){
+            num2.value = num2.value + btn.innerHTML
+            console.log(num2.value)
+        }
+        
+    });
+}
+
+
+
+
+putfDigit()
+
+
+// calcutes result with set operator
+// output on result field
 const operate = function(operator){
-    let result = document.getElementById("result")
-    x = parseInt(document.getElementById("x").value);
-    y = parseInt(document.getElementById("y").value);
+    
+    x = parseInt(document.getElementById("num1").value);
+    y = parseInt(document.getElementById("num2").value);
+    let result
+    console.log(x,y)
     switch (operator){
         case "+":
-            result.innerText = x + y;
+            result = x+y
+            document.getElementById("resi").value = result
             break;
         case "-":
-            result.innerText = x - y;
+            result = x-y
+            document.getElementById("resi").value = result
             break;
         case "/":
-            result.innerText = x / y;
+            result = x/y
+            document.getElementById("resi").value = result
             break;
         case "*":
-            result.innerText = x * y;   
+            result = x*y
+            document.getElementById("resi").value = result
             break;          
     }
 }
 
 const add = document.querySelector("#add")
-add.onclick = function(varx){
-    operate("+")
+
+//onclik function set the operator and switch to the second number for input
+add.onclick = function(){
+    op = "+"
+    putsDigit();
+    
     
     
 }
 
 const sub = document.querySelector("#sub")
-sub.onclick = function(varx){
-    operate("-")
+sub.onclick = function(){
+    op = "-"
+    putsDigit();
+    
     
     
 }
 
 const div = document.querySelector("#div")
-div.onclick = function(varx){
-    operate("/")
+div.onclick = function(){
+    op = "/"
+    putsDigit();
+    
     
     
 }
 
 const mul = document.querySelector("#mul")
-mul.onclick = function(varx){
-    operate("*")
-    
+mul.onclick = function(){
+    op = "*"
+    putsDigit();
     
 }
+
+
+//equals opertor calculates the result, gets numers from the now filled fields
+const eq = document.querySelector("#equ")
+    let res = document.getElementById("result") 
+
+    eq.onclick = function(result){
+        x = parseInt(document.getElementById("num1").value);
+        y = parseInt(document.getElementById("num2").value);
+
+        operate(op)
+        
+        
+    }
